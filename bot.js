e2eb44d49c5023e5bfb2985927dd3de4c0c9c593
palermodiaz <mahardiyanti@icloud.com>
2026-03-26 00:06:01 +0700

    disable mongo fix error

--- bot.js
+++ bot.js
@@ -2,13 +2,13 @@ require("dotenv").config();
 
 const { Telegraf } = require("telegraf");
 const OpenAI = require("openai");
-const mongoose = require("mongoose");
-const User = require("./user");
+//const mongoose = require("mongoose");
+//const User = require("./user");
 
 // connect DB
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB connected 🔥"))
-  .catch(err => console.log(err));
+//mongoose.connect(process.env.MONGO_URI)
+//  .then(() => console.log("MongoDB connected 🔥"))
+//  .catch(err => console.log(err));
 
 // init bot
 const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN);
